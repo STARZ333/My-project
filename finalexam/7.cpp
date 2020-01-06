17f9d7f97679dafb1a9a8ea6bf5293e96cb1bd05
STARZ333 <53653360+STARZ333@users.noreply.github.com>
2020-01-06 15:10:12 +0800

    Update 7.cpp

--- finalexam/7.cpp
+++ finalexam/7.cpp
@@ -7,7 +7,7 @@ int main()
     cin >> i;
     for (int j = 0; j < 15;j++)
     {
-        a[j] = rand() % 901 + 100;
+        a[j] = rand() % 9901 + 100;
         cout << a[j]<<endl;
     }
     cout << a[i - 1];
