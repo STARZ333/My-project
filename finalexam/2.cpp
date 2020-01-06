5f1cf7d4c9832e93f208b4af8efcc0337df35a2f
STARZ333 <53653360+STARZ333@users.noreply.github.com>
2020-01-06 18:16:51 +0800

    Update 2.cpp

--- finalexam/2.cpp
+++ finalexam/2.cpp
@@ -1,39 +1,38 @@
+#include <ctime>
 #include <iostream>
-#include<ctime>
 using namespace std;
 int main()
 {
-    int  arr[14], num;
+    int arr[14], num;
     string flower[] = {"S", "H", "D", "C"}, stnum[14];
     stnum[1] = 'A';
     stnum[11] = 'J';
     stnum[12] = 'Q';
     stnum[13] = 'K';
-    
+
     srand(time(0));
     //产生不重复随机数
     for (int i = 0; i < 13; i++)
     {
-        
-            arr[i] = rand() % 52 + 1;
-            for (int k = 0; k < i;k++)
+
+        arr[i] = rand() % 52 + 1;
+        for (int k = 0; k < i; k++)
+        {
+            if (arr[i] == arr[k])
             {
-                if (arr[i]==arr[k])
-                {
-                    i--;
-                    break;
-                }
+                i--;
+                break;
             }
+        }
     }
     //对随机数处理，输出花色和数字
     for (int i = 0; i < 13; i++)
     {
-        cout << flower[(arr[i]-1) / 13];
+        cout << flower[(arr[i] - 1) / 13];
         num = arr[i] % 13 + 1;
         if ((num == 1) || (num >= 11))
         {
-             cout << stnum[num];
-            
+            cout << stnum[num];
         }
         else
         {
