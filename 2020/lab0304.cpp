9cdc05f916171dfaf71ae2424e9095212813d37b
STARZ333 <53653360+STARZ333@users.noreply.github.com>
2020-03-13 11:51:32 +0800

    Update lab0304.cpp

--- 2020/lab0304.cpp
+++ 2020/lab0304.cpp
@@ -5,15 +5,20 @@ int main()
 {
     ofstream outfile;
     ifstream infile;
-    infile.open("1.txt");
-    outfile.open("2.txt");
-    char k,aft;
+    infile.open("1.in");
+    outfile.open("1.out");
+    char k;
     while (infile.get(k))
     {
-        if(k=='\n')
-            break;
-        aft = k ^ 'a';
-        outfile << aft;
+        if (k >= 'a' && k <= 'z')
+        {
+            k = k - 32;
+            outfile << k;
+        }
+        else
+        {
+            outfile << k;
+        }
     }
     outfile << "\n";
     infile.close();
